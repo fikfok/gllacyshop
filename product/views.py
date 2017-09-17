1f974bb644a55e5989a0b25061061093e689788c
fikfok <sadovenko.dmitry@gmail.com>
2017-09-18 00:09:42 +0300

    Коммит

--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Product
+from django.conf import settings
+from gllacyshop.settings import MEDIA_ROOT, MEDIA_URL
 
 # def products(request):
 #     return render(request, 'products.html', {'current_page': 'products'})
@@ -9,3 +11,8 @@ class ProductsListView(ListView):
     model = Product
     template_name = 'products.html'
     # paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['media_url'] = MEDIA_URL
+        return context
